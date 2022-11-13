21ce63b90f2e71153ba0f3c363292ccf4e23e6f2
north <weisf01@mingyuanyun.com>
2022-11-13 12:04:57 +0800

    feat(vite): vite base

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "north-spacex",
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
